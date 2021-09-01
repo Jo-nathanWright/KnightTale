7dea25a0f28192a1e64a282407d4c02473fa218c
ightTale <j.wright72127@gmail.com>
2021-09-01 16:01:02 -0600

    More Knight changes

--- Setup.sql
+++ Setup.sql
@@ -3,8 +3,19 @@ CREATE TABLE castles (
   name varchar(255) NOT NULL comment 'castle name',
   location varchar(255) NOT NULL comment 'castle location',
   creationYear int comment 'castle creation year'
-) default charset utf8 comment '';
+) default charset utf8;
+
+CREATE TABLE knights (
+  id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+  name varchar(255) NOT NULL comment 'knight name',
+  castleId int NOT NULL,
+  FOREIGN KEY (castleId) REFERENCES castles(id) ON DELETE CASCADE
+);
+
+INSERT INTO knights (name, castleId) VALUES ("Lance A Little", 1);
 
 SELECT * FROM castles;
+
+SELECT * FROM knights WHERE castleId = 1;
 
 INSERT INTO castles (name, location, creationYear) VALUES ('Stone Chester', 'Ireland', 1200)
