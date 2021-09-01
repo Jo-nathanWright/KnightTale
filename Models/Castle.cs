57e647fd938a7dfe35fc739e3a4d602b3e5468cb
ightTale <j.wright72127@gmail.com>
2021-09-01 13:56:59 -0600

    Castle Model Updates

--- Models/Castle.cs
+++ Models/Castle.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace KnightTale.Models
 {
     public class Castle
     {
-        
-    }
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public int CreationYear { get; set; }
+  }
 }
